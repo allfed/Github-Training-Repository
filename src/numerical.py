07e17321bf2886d765cf8dba3f8712e8b3326c67
lzhangUT <39503235+lzhangUT@users.noreply.github.com>
2022-12-13 13:22:03 +0800

    first commit
    set up the folders, python and notebook

---
+++ src/numerical.py
@@ -0,0 +1,16 @@
+def add_numbers(a = 7,  b = 8):
+    """
+    add the numbers together
+
+    Arguments:
+        a: one number
+        b: the other number
+
+    Returns:
+        float: the addition of the two numbers
+    """
+
+    sum = float(a) + float(b)
+    print('Sum:', str(sum))
+
+
